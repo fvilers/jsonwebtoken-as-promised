590f9f87e166ec30cacfed5db5fc23d5bc880d71
Fabian Vilers <fabian.vilers@dev-one.com>
2016-12-03 19:18:09 +0100

    Add jwt errors

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = {
   decode: require("./src/decode"),
   verify: require("./src/verify"),
-  sign: require("./src/sign")
+  sign: require("./src/sign"),
+  JsonWebTokenError: jwt.JsonWebTokenError,
+  NotBeforeError: jwt.NotBeforeError,
+  TokenExpiredError: jwt.TokenExpiredError
 };
