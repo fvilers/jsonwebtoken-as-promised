41fe345b43f0b99114ea9c71396cf4b0a97987d9
Fabian Vilers <fabian.vilers@dev-one.com>
2016-12-04 09:13:10 +0100

    Add verify tests

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+"use strict";
+
 const jwt = require("jsonwebtoken");
+
+Promise.prototype["finally"] = function (f) {
+  return this.then(function (value) {
+    return Promise.resolve(f()).then(function () {
+      return value;
+    });
+  }, function (err) {
+    return Promise.resolve(f()).then(function () {
+      throw err;
+    });
+  });
+};
 
 module.exports = {
   decode: require("./src/decode"),
